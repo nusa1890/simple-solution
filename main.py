a0185effe17677aeaaca22810b8f529aa1d586ef
muhammad-fahmi <fahmipradana4@gmail.com>
2021-06-03 07:32:17 +0800

    update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import flask
 import googleapiclient.discovery
 import numpy as np
 import os, json
+from numpy import array
 from google.api_core.client_options import ClientOptions
 from flask import Flask, request, jsonify
 
 def predict_json(project, region, model, instances, version=None):
     # Create the ML Engine service object.
     # To authenticate set the environment variable
-    #os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/nusa/Documents/euphoric-fusion-312609-e20f53f87718.json"
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/nusa/Documents/euphoric-fusion-312609-e20f53f87718.json"
 
     prefix = "{}-ml".format(region) if region else "ml"
     api_endpoint = "https://{}.googleapis.com".format(prefix)
@@ -38,7 +39,7 @@ def index():
     project = request.args.get('project')
     region = request.args.get('region')
     model = request.args.get('model')
-    instances = [[[0.24157303, 0.29166667, 0.2238806, 0.54140127, 0.19047619, 0.54140127], [0.34831461, 0.41666667, 0.41044776, 0.57961783, 0.23809524, 0.57961783]]]
+    instances = json.loads(request.args.get('instances'))
     version = request.args.get('version')
 
     x = predict_json(project, region, model, instances, version)
